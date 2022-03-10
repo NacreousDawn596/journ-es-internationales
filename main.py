cba292068566f9342c679185e575437872e1c57f
NacreousDawn596 <75474839+NacreousDawn596@users.noreply.github.com>
2022-03-10 19:16:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
 import json, os;
-for event in json.loads(open("événements.json", "r").read())[os.popen("date").read().split()[2]][f'{int(os.popen("date").read().split()[1]) + 0}']: print(event);
+try:
+	for event in json.loads(open("événements.json", "r").read())[os.popen("date").read().split()[2]][f'{int(os.popen("date").read().split()[1]) + 0}']: print(event);
+except:
+	print("il n y a aucun événement mondial aujourd'hui :/") 
